1f68a5b1d044f8a828519988fbdbae422aa13ef0
fady231 <fadyraouf231@gmail.com>
2023-04-14 15:02:05 +0200

    100%

---
+++ ServeSide/functions/index.js
@@ -0,0 +1,48 @@
+const functions = require("firebase-functions");
+
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const student = require("./routes/students");
+const parents = require("./routes/parents");
+const FSE = require("./routes/DaTa");
+const data = require("./routes/dataRouter")
+const Task = require("./routes/Task");
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/FSEimage',express.static(path.join('FSEimage')));
+app.use('/StudentProfilePic',express.static(path.join('StudentProfilePic')));
+app.use('/Profile',express.static(path.join('Profile')));
+app.use('/ParentProfilePic',express.static(path.join('ParentProfilePic')));
+app.use("/student", student);
+app.use("/parent", parents);
+app.use("/FSE", FSE);
+app.use("/Task", Task);
+app.use("/data", data);
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlparser: true,
+    useUnifiedTopology: true,
+  }, () => console.log("connected to DB!"));
+  
+/*
+mongoose.connect(
+ //"mongodb://localhost:27017",
+ "mongodb+srv://ahmed:7IxlE70oZXGK8WVz@cluster0.urdmbgm.mongodb.net/questions?retryWrites=true&w=majority",
+  {
+    useNewUrlparser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to DB!")
+);
+*/
+app.listen(process.env.PORT || 3000);
+
+//app.listen(3000);
+exports.app = functions.https.onRequest(app);
