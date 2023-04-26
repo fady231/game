354c71ddf6a70e753ed95cc2f561e06b3ae0e608
Fady Raouf <100470798+fady231@users.noreply.github.com>
2023-04-26 18:05:44 +0200

    Update DaTa.js

--- ServeSide/routes/DaTa.js
+++ ServeSide/routes/DaTa.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 
 const control = require('../controller/DataConrtoller');
-const { updateMany } = require('../models/DaTadb');
+const { updateMany } = require('../models/Datadb');
 
 
 
